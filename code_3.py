b193ab354d1f4f2a0fe51881732a609308dd22ff
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2023-12-03 00:10:29 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,42 +1,27 @@
-    return data
-    data = generate_random_data()
 
-    main()
-
+def generate_random_data():
 if __name__ == "__main__":
-def main():
     data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
+    data = generate_random_data()
 import random
         print(f"Random Number: {item}")
 
+    return data
 
-def generate_random_data():        print(f"Random Number: {item}")
-    main()
 def main():
 
-    data = generate_random_data()
-if __name__ == "__main__":
+    main()
+    for item in data:import random
 
-import random
+        print(f"Random Number: {item}")
+    return data
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
 def generate_random_data():
 
-    data = [random.randint(1, 100) for _ in range(10)]
+
     for item in data:
-    return data
-function main() {
-}
-    });
-    const data = [];
-function generateRandomData() {
-    for (let i = 0; i < 10; i++) {
-
-    const data = generateRandomData();
-    return data;
-
-}
-main();
-    }
-        console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
+if __name__ == "__main__":
+def main():
+    main()
